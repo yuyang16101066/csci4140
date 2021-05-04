5cfcc914e6c448dd9d92f064aaa6bbe0386ed771
yuyang16101066 <41040791+yuyang16101066@users.noreply.github.com>
2021-05-04 21:06:28 +0800

    Add files via upload

---
+++ visulize_range.py
@@ -0,0 +1,18 @@
+import re
+
+string = "2020.05"
+pattern = "^(\d{1,4})\.(\d{1,2})$"
+m = re.search(pattern, string)
+year = int(m.groups()[0])
+month = int(m.groups()[1])
+if m:
+    if 1 <= year <= 3000 and 1 <= month <= 12:
+        print("Valid pair")
+        if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+            print("The year is a leap year")
+        else:
+            print("The year is not a leap year")
+    else:
+        print("Invalid pair")
+else:
+    print("Invalid pair")
